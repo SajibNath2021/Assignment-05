a239d73fc8a00f973a8f6f0e6dab7997d31c82a3
Sajib Nath <sajibnath1996@gmail.com>
2021-02-07 17:58:18 +0600

    finaly it work

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-const foodssDiv = document.getElementById('foods').innerText;
+
 function displayFood() {
+    const foodssDiv = document.getElementById('foods').innerText;
     const inputFood = document.getElementById('input-foodName').value;
    
     fetch(` https://www.themealdb.com/api/json/v1/1/search.php?f=${inputFood}`)
    
     .then(res => res.json())
-    .then(data => displayFood(data.meals))
-     const displayFood = meals=>{
-            console.log(meals[0].strMeal);
-            document.getElementById('foods').innerText = meals[0].strMeal;
-    //     for (let i = 0; i <= meals.length; i++) {
-             
-    //         const mealsName = meals[i].strMeal;
-                     
-    //        console.log(mealsName);
-    //     //    document.getElementById('foods').value = mealsName;
-    //     }
-      }
-
-
-
-
-
-    //     { //console.log(data.meals[0].strMeal)
-        
-    //      for (let i = 0; i <= data.meals.length; i++) {
-             
-    //          const mealsName = data.meals[i];
-             
-    //          console.log(mealsName.strMeal);
-    //         //  document.getElementById('input-foodName').value = mealsName.strMeal;
-    //     //     const foodsDiv = document.createElement('div');
-    //     //     const foodsInfo = `
-    //     //     <h3>${mealsName.strMeal}</h3>
-    //     //     `;
-    //     //     foodsDiv.innerHTML = foodsInfo;
-    //     //     foodsDiv.appendChild(foodsDiv);
+    .then(data => display(data.meals))
+     
+    function  display(meals){
+        for (let i = 0; i < meals.length; i++) {
+            const mealsName = meals[i];
+            console.log(mealsName.strMeal);
+            document.getElementById('foods').innerText = mealsName.strMeal ;
             
-    //      }
-    //  } );
-}
+        }
+    }
+
+    //  const displayFood = meals=>{
+         
+    //         console.log(meals[0].strMeal);
+    //     //    document.getElementById('foods').innerText  = meals[0].strMeal;
+    
+    
+    
+    
+           
+             
+    //          const mealsName = meals.strMeal;
+    //          console.log(mealsName);
+    //        //  document.getElementById('foods').innerText = mealsName;
+             
+             
+        
+        //  for (let i = 0; i <= meals.length; i++) {
+        //         const mealsName = meals[i].strMeal;
+        //        console.log(mealsName);
+        //     document.getElementById('foods').innerText = mealsName;
+        //     // const foodsDiv = document.createElement('div');
+        //     // const foodsInfo = `
+        //     // <h3>${mealsName.strMeal}</h3>
+        //     // `;
+        //     // foodsDiv.innerHTML = foodsInfo;
+        //     // foodssDiv.appendChild(foodsDiv);
+            
+         }
+     
+// }
+

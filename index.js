5456ba80be9acab5d4cd2278c1d97ee085f89ef7
Sajib Nath <sajibnath1996@gmail.com>
2021-02-07 21:05:44 +0600

    50 marks done

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 
 function displayFood() {
-    const foodssDiv = document.getElementById('foods');
+    const allFoodsDiv = document.getElementById('foods');
     const inputFood = document.getElementById('input-foodName').value;
    
     fetch(` https://www.themealdb.com/api/json/v1/1/search.php?f=${inputFood}`)
-   
     .then(res => res.json())
     .then(data => display(data.meals))
      
-    function  display(meals){
-        for (let i = 0; i < meals.length; i++) {
-            const mealsName = meals[i];
-            console.log(mealsName.strMeal);
-            
-            
-             
+    const  display = meals=>{
+           
+        meals.forEach(mealsName => {
             const foodsDiv = document.createElement('div');
             foodsDiv.className = "food";
             const foodsInfo = `
@@ -23,14 +18,15 @@ function displayFood() {
              <button onclick = "displayFoodDetails('${mealsName.idMeal}')" >details</button>
             `;
             foodsDiv.innerHTML = foodsInfo;
-            foodssDiv.appendChild(foodsDiv);
-           
-        }
+            allFoodsDiv.appendChild(foodsDiv);
+        });
+       
     }
+    
      
 }
 
-   function displayFoodDetails(id){
+   const displayFoodDetails= id =>{
       const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
       fetch(url)
       .then(res => res.json())
@@ -38,13 +34,22 @@ function displayFood() {
       
     }
 
-    function DisplayFoodsInfo(food){
+   const  DisplayFoodsInfo = food =>{
          const foodDiv = document.getElementById('foodDetails');
+         foodDiv.className = 'detail'
          foodDiv.innerHTML =`
          <img src = ${food.strMealThumb}>
          <h3>${food.strMeal}</h3>
-         
-
+         <p>Ingredient No1 : ${food.strIngredient1}<p>
+         <p>Ingredient No2 : ${food.strIngredient2}<p>
+         <p>Ingredient No3 : ${food.strIngredient3}<p>
+         <p>Ingredient No4 : ${food.strIngredient4}<p>
+         <p>Ingredient No5 : ${food.strIngredient5}<p>
+         <p>Ingredient No6 : ${food.strIngredient6}<p>
+         <p>Ingredient No7 : ${food.strIngredient7}<p>
+         <p>Ingredient No8 : ${food.strIngredient8}<p>
+         <p>Ingredient No9 : ${food.strIngredient9}<p>
+         <p>Ingredient No10 : ${food.strIngredient10}<p>
          `
     }
 
